c4465fd4aecf115e85c50aa758debae2eb9fef05
Thanashs Kyriakopoulos <thanashs.kyr@gmail.com>
2023-05-07 20:18:21 +0300

    Story20 - Added the Employee and Department SQL queries

--- db_queries/USERS.sql
+++ db_queries/USERS.sql
@@ -42,3 +42,61 @@ VALUES
 
 
 SELECT * FROM TOKENS;
+
+
+-- Departments and Employees
+
+DROP TABLE Department CASCADE;
+DROP TABLE Employee CASCADE;
+DROP TABLE Employee_Department CASCADE;
+
+
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  uin_number VARCHAR(255) NOT NULL,
+  department_id INTEGER REFERENCES department(id)
+);
+
+CREATE TABLE employee_department (
+  employee_id INTEGER REFERENCES employee(id),
+  department_id INTEGER REFERENCES department(id),
+  PRIMARY KEY (employee_id, department_id)
+);
+
+INSERT INTO department (name) VALUES ('IT');
+INSERT INTO department (name) VALUES ('Sales');
+INSERT INTO department (name) VALUES ('HR');
+
+INSERT INTO employee (name, surname, uin_number, department_id) 
+VALUES ('John', 'Doe', '12345', 1);
+
+INSERT INTO employee (name, surname, uin_number, department_id) 
+VALUES ('Jane', 'Doe', '67890', 2);
+
+INSERT INTO employee (name, surname, uin_number, department_id) 
+VALUES ('Bob', 'Smith', '24680', 3);
+
+INSERT INTO employee_department (employee_id, department_id) VALUES (1, 1);
+INSERT INTO employee_department (employee_id, department_id) VALUES (2, 2);
+INSERT INTO employee_department (employee_id, department_id) VALUES (2, 1);
+INSERT INTO employee_department (employee_id, department_id) VALUES (3, 3);
+
+
+
+select * from employee;
+select * from department;
+select * from employee_department;
+
+SELECT e.name, e.surname, e.uin_number
+FROM employee e
+JOIN employee_department de ON e.id = de.employee_id
+JOIN department d ON de.department_id = d.id
+WHERE d.name = 'IT';
