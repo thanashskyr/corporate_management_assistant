1cd3cfb590ca8d8b0c8842d50aa07240feaa54d7
Thanashs Kyriakopoulos <thanashs.kyr@gmail.com>
2023-05-26 14:36:47 +0300

    Improved Login Form UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,36 @@
 import React from "react";
-import Login from "./login";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Employees from "./Employee";
-import PrivateRoute from './Auth';
+import PrivateRoute from "./Auth";
 import Departments from "./Department";
-import './theme.css';
+import "./theme.css";
 
 function App() {
-    
-    
   return (
-    
     <Router>
       <div className="body-background">
         <div className="App">
-       
-              <div className="content">
-                  <Switch>
-                      <Route exact path="/" component={Login}/>
-                      <PrivateRoute
-                          exact
-                          path="/dashboard"
-                          component={Dashboard}
-                      />
-                      <PrivateRoute exact path="/Employee" component={Employees}></PrivateRoute>
-                      <PrivateRoute exact path="/Department" component={Departments}></PrivateRoute>
-                      
-                  </Switch>
-              </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/Employee"
+                component={Employees}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/Department"
+                component={Departments}
+              ></PrivateRoute>
+            </Switch>
+          </div>
         </div>
       </div>
     </Router>
-   
   );
 }
 
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,91 +1,116 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import { Box, TextField, Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
+import cmaLogo from "./cmaLogoNoBG.png";
 
-function Login() {
-  
-  const [username, setUsername]= useState('');
-  const [password, setPassword]= useState('');
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   // const [token, setToken]= useState('');
   const history = useHistory();
 
-  const handleUsernameChange = (event) =>{
+  const handleUsernameChange = (event) => {
     const value = event.target.value;
     setUsername(value);
-  }
+  };
 
-  const handlePasswordChange = (event) =>{
+  const handlePasswordChange = (event) => {
     const value = event.target.value;
     setPassword(value);
-  }
+  };
 
+  const handleLogin = () => {
+    const data = {
+      username: username,
+      password: password,
+    };
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Invalid credentials");
+        }
+      })
+      .then((data) => {
+        try {
+          const receivedToken = data.token;
+          localStorage.setItem("token", receivedToken);
+          const expirationTime = new Date().getTime() + 24 * 60 * 60 * 1000; //after 24 hours
+          localStorage.setItem("expirationTime", expirationTime.toString());
 
-  const handleLogin = () =>{
+          // setToken(receivedToken);
 
-        const data = {
-            username: username,
-            password: password 
-        };
-        fetch('http://localhost:3000/login',{
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            throw new Error('Invalid credentials');
+          const storedToken = localStorage.getItem("token");
+          console.log("Token:", storedToken);
+          console.log("Response:", data.message);
+          if (data.token) {
+            history.push("/dashboard");
           }
-        })
-        .then((data) => {
-
-            try{
-              const receivedToken= data.token;
-              localStorage.setItem('token', receivedToken);
-              const expirationTime = new Date().getTime() + 24 * 60 * 60 * 1000;//after 24 hours
-              localStorage.setItem('expirationTime', expirationTime.toString());
-
-              // setToken(receivedToken);
-
-              const storedToken = localStorage.getItem('token');
-              console.log("Token:", storedToken);
-              console.log("Response:", data.message);
-              if (data.token)
-              { 
-                history.push("/dashboard");
-              }
-            }catch(error){
-              console.log('Unauthorised');
-            }
-
-        })
-        .catch((error) => {
-          console.error("Error1:", error);
-        
-        });
-    
-       
+        } catch (error) {
+          console.log("Unauthorised");
+        }
+      })
+      .catch((error) => {
+        console.error("Error1:", error);
+      });
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-        <div>
-        <h1>Welcome to Corporation Management App</h1>  
-        <h2>Login</h2>
-        <TextField label="Username" variant="filled" style={{ width: '300px', height: '55px' }} value = { username } onChange={handleUsernameChange}/>
-        <br/>
-        <TextField type="password" label="Password" variant="filled" style={{ width: '300px', height: '55px' }} value = { password } onChange={handlePasswordChange}/>
-        <br/>
-        <Button variant="contained" color="primary" style={{ width: '120px', height: '55px' }} onClick={handleLogin}>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box textAlign="center" sx={{ padding: "20px", bottom: "200px" }}>
+        <img src={cmaLogo} alt="Logo" />
+        <h1>Welcome to Corporation Management App</h1>
+      </Box>
+
+      <Box
+        textAlign="center"
+        sx={{ backgroundColor: "white", borderRadius: "10px", padding: "50px" }}
+      >
+        <h1>Login</h1>
+        <Box mb={2}>
+          <TextField
+            label="Username"
+            variant="filled"
+            style={{ width: "300px", height: "55px" }}
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            type="password"
+            label="Password"
+            variant="filled"
+            style={{ width: "300px", height: "55px" }}
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </Box>
+        <Box mb={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ width: "120px", height: "55px" }}
+            onClick={handleLogin}
+          >
             Log in!
-        </Button>
-        </div>
-    </div>
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default Login;
